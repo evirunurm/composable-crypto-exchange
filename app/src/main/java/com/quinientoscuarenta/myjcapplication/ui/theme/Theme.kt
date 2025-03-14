6b13dd1df6d73a6a22fdeaec92472acad6092f1c
Evelin Virunurm <evelinviru@gmail.com>
2025-03-14 12:23:54 +0100

    Explore color classes structure.

--- app/src/main/java/com/quinientoscuarenta/myjcapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/quinientoscuarenta/myjcapplication/ui/theme/Theme.kt
@@ -6,28 +6,62 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-
+// These classes would, ideally, have a structure similar to:
+// - Generic
+//      - Transparent: Color
+//      - White: Color
+//      - Black: Color
+//      - Soft: Color
+// - Brand
+// - Secondary
+// - Neutral
+// - Red
+// ...
 @Immutable
 data class ColorScheme(
     val genericWhite: Color,
     val genericBlack: Color,
     val brand100: Color,
-    val brand200: Color,
+    val brand900: Color, // Real example
     val neutral100: Color,
     val secondary100: Color,
     val red100: Color
 )
 
+// This class would, ideally, have a structure similar to:
+// - Generic
+//      - Transparent: Color
+// - Text
+//      - Default: Color
+//      - Subtle01: Color
+//      - Links: Color
+//      - Brand01: Color
+//      - Brand02: Color
+//      - Disabled: Color
+//      - Inverse: Color
+// - Icon
+// - Container Background
+// - Page Background
+// ...
 @Immutable
-class SemanticColors(
-    val globalWhite: Color,
-    val textDefault: Color,
-    val textSubtle: Color,
+data class SemanticColors(
+    val containerBackgroundBrand01: Color,
+    val containerBackgroundDisabled: Color,
+    val supportiveFeedbackError: Color,
+    val supportiveFeedbackSuccess: Color,
     val textLinks: Color,
-    val iconDefault: Color,
-    val containerBackgroundDefault: Color
 )
 
+// This class would, ideally, have a structure similar to:
+// - Button
+//      - Background
+//          - Primary
+//              - Default: Color
+//              - Disabled: Color
+//              - Error: Color
+//          - Secondary
+// - Icon
+// - Input
 @Immutable
 data class ComponentColors(
     val buttonBackgroundPrimaryDefault: Color,
@@ -47,7 +81,7 @@ fun coreColorsScheme(): ColorScheme {
         neutral100 = Color(0xFF000000),
         secondary100 = Color(0xFF000000),
         red100 = Color(0xFF000000),
-        brand200 = Color(0xFF4E55F1)
+        brand900 = Color(0xFF000D44)
     )
 }
 
@@ -58,13 +92,14 @@ fun customColorsScheme(): ColorScheme {
     val coreColors = coreColorsScheme()
 
     return ColorScheme(
+        // These would be the overrides
         genericWhite = Color(0xFFFFFFFF),
         genericBlack = Color(0xFF000000),
         brand100 = Color(0xFF2D2121),
         neutral100 = Color(0xFF333333),
         secondary100 = Color(0xFF4BABB4),
         red100 = Color(0xFF000000),
-        brand200 = coreColors.brand200
+        brand900 = coreColors.brand900 // Real example
     )
 }
 
@@ -75,27 +110,24 @@ fun semanticColorsScheme(
     colorScheme: ColorScheme = customColorsScheme()
 ): SemanticColors {
     return SemanticColors(
-        globalWhite = colorScheme.genericWhite,
-        textDefault = colorScheme.genericWhite,
-        textSubtle = colorScheme.genericWhite,
-        textLinks = colorScheme.brand100,
-        iconDefault = colorScheme.genericWhite,
-        containerBackgroundDefault = colorScheme.neutral100
+        containerBackgroundBrand01 = colorScheme.brand900,
+        containerBackgroundDisabled = colorScheme.neutral100,
+        supportiveFeedbackError = colorScheme.red100,
+        supportiveFeedbackSuccess = colorScheme.secondary100,
+        textLinks = colorScheme.brand900
     )
 }
 
 fun componentColorsScheme(
-    // ?? Provide by default. Not sure if this makes sense.
-    // Just so it can be called without parameters during LocalSemanticColors initialization.
-    colorScheme: ColorScheme = customColorsScheme()
+    semanticColors: SemanticColors = semanticColorsScheme()
 ): ComponentColors {
     return ComponentColors(
-        buttonBackgroundPrimaryDefault = colorScheme.brand100,
-        buttonBackgroundPrimaryDisabled = colorScheme.brand100,
-        buttonBackgroundPrimaryError = colorScheme.red100,
-        buttonBackgroundSecondaryDefault = colorScheme.secondary100,
-        buttonBackgroundSecondaryDisabled = colorScheme.secondary100,
-        buttonBackgroundSecondaryError = colorScheme.red100
+        buttonBackgroundPrimaryDefault = semanticColors.containerBackgroundBrand01, // Real examples
+        buttonBackgroundPrimaryDisabled = semanticColors.textLinks,
+        buttonBackgroundPrimaryError = semanticColors.textLinks,
+        buttonBackgroundSecondaryDefault = semanticColors.textLinks,
+        buttonBackgroundSecondaryDisabled = semanticColors.textLinks,
+        buttonBackgroundSecondaryError = semanticColors.textLinks
     )
 }
 
@@ -108,9 +140,11 @@ fun GenuineTheme(
     customColors: ColorScheme,
     content: @Composable () -> Unit
 ) {
-    // With the passed custom colors, generate the semantic, component, state, etc colors.
+    // With the passed custom colors, generate the semantic.
     val semanticColors = semanticColorsScheme(customColors)
-    val componentColors = componentColorsScheme(customColors)
+    // Component colors are based on the semantic colors.
+    val componentColors = componentColorsScheme(semanticColors)
+    // val componentStateColors = componentStateColorsScheme(customColors)
 
     CompositionLocalProvider(
         LocalCustomColors provides customColors,
